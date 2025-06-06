89c7059885a08cf46b30f96079eef468fff3c201
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2025-06-07 06:15:43 +0900

    feat(vscode): implement ts-md extension

---
+++ packages/vscode/src/extension.ts
@@ -0,0 +1,45 @@
+import type * as vscode from 'vscode';
+import {
+  LanguageClient,
+  type LanguageClientOptions,
+  type ServerOptions,
+  TransportKind,
+} from 'vscode-languageclient/node';
+import { registerCodeLens } from './codelens';
+import { registerCommands } from './commands';
+
+let client: LanguageClient;
+
+export async function activate(ctx: vscode.ExtensionContext) {
+  const serverModule = ctx.asAbsolutePath('dist/server.js');
+
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.stdio },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.stdio,
+      options: { execArgv: ['--inspect=6009'] },
+    },
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ language: 'ts-md' }],
+    progressOnInitialization: true,
+  };
+
+  client = new LanguageClient(
+    'ts-md-ls',
+    'TS-MD Language Server',
+    serverOptions,
+    clientOptions,
+  );
+  await client.start();
+  ctx.subscriptions.push({ dispose: () => client.stop() });
+
+  registerCodeLens(ctx);
+  registerCommands(ctx, client);
+}
+
+export function deactivate() {
+  return client?.stop();
+}
