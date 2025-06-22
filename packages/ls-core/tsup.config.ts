f937194cb036ae002c9c4146260c77d55368fc80
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2025-06-22 12:29:49 +0900

    chore: drop paths from editor tsconfigs (#147)

--- packages/ls-core/tsup.config.ts
+++ packages/ls-core/tsup.config.ts
@@ -1,10 +1,12 @@
+import tsMd from '@sterashima78/ts-md-unplugin/esbuild';
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: true,
+  dts: false,
   clean: true,
   target: 'node18',
   external: ['typescript'],
+  esbuildPlugins: [tsMd],
 });
