cb759e4378ad6f4c6fc2d4b2f0e0bad9945c4604
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2025-06-22 20:24:28 +0900

    test(loader): move tests to ts.md (#151)

---
+++ packages/loader/vitest.config.ts
@@ -0,0 +1,10 @@
+import tsMd from '@sterashima78/ts-md-unplugin/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsMd],
+  test: {
+    globals: true,
+    include: ['src/index.ts.md'],
+  },
+});
