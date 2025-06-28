c441e62656cf9822ce3184f8136735ffa2a71b95
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2025-06-28 12:38:50 +0900

    docs: add playground page with monaco editor

--- packages/docs/astro.config.mjs
+++ packages/docs/astro.config.mjs
@@ -1,14 +1,25 @@
+import react from '@astrojs/react';
 import starlight from '@astrojs/starlight';
 // @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   base: '/ts-md/',
+  vite: {
+    ssr: {
+      external: [
+        'monaco-editor',
+        '@volar/monaco',
+        '@sterashima78/ts-md-monaco',
+      ],
+    },
+  },
   integrations: [
     starlight({
       title: 'TS-MD Docs',
       sidebar: [{ label: 'Packages', autogenerate: { directory: 'packages' } }],
       markdown: { headingLinks: false },
     }),
+    react(),
   ],
 });
