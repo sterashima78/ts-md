dd74cb3a51f3e841e277d01a0d1fd7f084d910ed
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-07-30 23:42:57 +0900

    style(core): organize public exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,16 +1,10 @@
 export { detectCycle } from './graph.ts.md';
+export type { TsMdModuleId } from './module-id.ts.md';
 export {
   createVirtualModuleFileName,
   parseVirtualModuleFileName,
   TS_MD_VIRTUAL_MODULE_MARKER,
 } from './module-id.ts.md';
-export type { TsMdModuleId } from './module-id.ts.md';
-export {
-  parseChunkInfos,
-  parseChunks,
-  parseDocument,
-  TsMdParseError,
-} from './parser.ts.md';
 export type {
   ChunkDict,
   ChunkInfo,
@@ -18,7 +12,13 @@ export type {
   TsMdLanguage,
   TsMdModule,
 } from './parser.ts.md';
-export { cleanImporter, resolveImport } from './resolver.ts.md';
+export {
+  parseChunkInfos,
+  parseChunks,
+  parseDocument,
+  TsMdParseError,
+} from './parser.ts.md';
 export type { ResolvedTsMdImport } from './resolver.ts.md';
+export { cleanImporter, resolveImport } from './resolver.ts.md';
 export { tangle } from './tangle.ts.md';
 export * from './utils.ts.md';
