6c6aad0ecc6f5d160c9930c92a0aafd9ce03dd72
sterashima78 <45279563+sterashima78@users.noreply.github.com>
2026-08-28 22:04:15 +0900

    feat: export content mapper transform

---
+++ packages/content-mapper/src/index.ts
@@ -0,0 +1,13 @@
+export type {
+  InitializeParams,
+  InitializeResult,
+  MappedOutput,
+  MapperDiagnostic,
+  PositionEncoding,
+  SourceExtension,
+  SpanMapping,
+  TransformParams,
+  TransformResult,
+} from './protocol.js';
+export { SpanMapKind } from './protocol.js';
+export { DIAGNOSTIC_SOURCE, transformTsMd } from './transform.js';
